3e3c740ab63b2fc07ef9c2eb8423a8ac51608123
Jonathan <jalmeida0291@gmail.com>
2018-10-27 17:18:19 -0400

    fixed dropdown display for mobile

---
+++ js/app.js
@@ -0,0 +1,45 @@
+(function() {
+  "use strict";
+
+  scrollToViewLink("about");
+  scrollToViewLink("projects");
+  scrollToViewLink("contact");
+
+  transitionNavBar();
+
+  function scrollToViewLink(page) {
+    document.getElementById(`${page}-link`).addEventListener("click", e => {
+      // Need to prevent default behavior of link in order for smooth transition
+      e.preventDefault();
+      document.getElementById(page).scrollIntoView({ behavior: "smooth" });
+    });
+  }
+
+  function transitionNavBar() {
+    document.querySelector(".burger").addEventListener("click", e => {
+      transformBurger();
+      if (document.querySelector(".nav").classList.contains("block-js")) {
+        dropUpMenu();
+      } else {
+        dropDownMenu();
+      }
+    });
+  }
+
+  function transformBurger() {
+    document.querySelector(".burger").classList.toggle("transform");
+  }
+
+  function dropDownMenu() {
+    document.querySelector(".nav").classList.toggle("block-js");
+    setTimeout(() => {
+      document.querySelector(".nav-list").classList.toggle("transform-zero-js");
+    }, 0);
+  }
+  function dropUpMenu() {
+    document.querySelector(".nav-list").classList.toggle("transform-zero-js");
+    setTimeout(() => {
+      document.querySelector(".nav").classList.toggle("block-js");
+    }, 500);
+  }
+})();
